9df7b308aa2a765c829800ecb9c4472afe1c29c7
sreedharkr <sreedharkr@gmail.com>
2017-12-28 21:33:41 -0700

    new branch

--- boston1.py
+++ boston1.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 import pandas as pd
 boston_data = datasets.load_boston()
+print('testing')
 dir(boston_data)
 bdf = np.c_[boston_data.data, boston_data.target]
 bdf_features = np.append(boston_data.feature_names,np.array(['target']))
